0a165446c4c93700fbf862fde88f0e93da01881d
prem-shivarathri <78019495+prem-shivarathri@users.noreply.github.com>
2021-06-04 19:03:45 +0530

    Add files via upload

---
+++ Assignment.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/nav/Navbar'
+import Home from './components/Home'
+
+import {BrowserRouter as Router} from 'react-router-dom'
+import {Route,Switch} from 'react-router-dom'
+import About from './components/About'
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
